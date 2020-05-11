3e1b1395c1a1a92b5086bedf3398d2190d95a06c
FitzSilk <60465804+FitzSilk@users.noreply.github.com>
2020-05-11 15:31:45 +0200

    Creation of Story1: register - update not working.

--- youcoach-backend/src/main/java/com/switchfully/youcoach/domain/users/UserRepository.java
+++ youcoach-backend/src/main/java/com/switchfully/youcoach/domain/users/UserRepository.java
@@ -1,7 +1,10 @@
 package com.switchfully.youcoach.domain.users;
 
-import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
+@Repository
 public interface UserRepository extends CrudRepository<User, UUID> {
 }
