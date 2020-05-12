e94e9ad75a3212610868208f0e24191cf3f9de93
Patrizia Battisti <patriziabattisti@hotmail.com>
2020-05-12 13:59:41 +0200

    registration component

--- youcoach-frontend/src/app/registration/registration.component.ts
+++ youcoach-frontend/src/app/registration/registration.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {UserService} from '../services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  error;
+  success;
+  registerForm;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) {
+    this.registerForm = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit(loginData) {
+    this.success = false;
+    this.error = false;
+    // this.userService.
+    this.registerForm.reset();
+  }
 }
