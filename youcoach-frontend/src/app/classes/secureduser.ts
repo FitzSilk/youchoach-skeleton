daaa5ee1c04abc52076991e39fc8ef094b5016ea
FitzSilk <60465804+FitzSilk@users.noreply.github.com>
2020-05-13 12:37:02 +0200

    Register-front-end: Testing if it works.

--- youcoach-frontend/src/app/classes/secureduser.ts
+++ youcoach-frontend/src/app/classes/secureduser.ts
@@ -1,5 +1,13 @@
-export interface SecuredUser {
+export class SecuredUser {
   id: number;
   username: string;
   password: string;
+  roles: number;
+
+
+  constructor(username: string, password: string, roles: number) {
+    this.username = username;
+    this.password = password;
+    this.roles = roles;
+  }
 }
