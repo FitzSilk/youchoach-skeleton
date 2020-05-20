bb0a2a1a74f9e6c97a19c7dbd6953ce02dbd9dda
TWENS-VDAB <thomas.wens@vdab.be>
2020-05-20 10:58:44 +0200

    started on listing coaches in front end

---
+++ youcoach-frontend/src/app/coaches-overview/coaches-overview.component.ts
@@ -0,0 +1,28 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from "../classes/user";
+import {UserService} from "../services/user.service";
+
+@Component({
+  selector: 'app-coaches-overview',
+  templateUrl: './coaches-overview.component.html',
+  styleUrls: ['./coaches-overview.component.css']
+})
+export class CoachesOverviewComponent implements OnInit {
+
+  users: User[];
+
+  constructor(private userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.getCoaches();
+
+  }
+
+
+  getCoaches(): void {
+    this.userService.getCoaches().subscribe(user => this.users = user);
+    console.log(this.users);
+  }
+
+}
