ecb94f03097dd87b9ae7378cdc801ada557660e9
Patrizia Battisti <patriziabattisti@hotmail.com>
2020-05-26 14:30:58 +0200

    trying to fix the filtering

--- youcoach-frontend/src/app/coaches-overview/coaches-overview.component.ts
+++ youcoach-frontend/src/app/coaches-overview/coaches-overview.component.ts
@@ -15,6 +15,7 @@ export class CoachesOverviewComponent implements OnInit {
   allTheCoaches: User[];
   user: User;
   option: string;
+  optionYear: string;
   topics = ['French', 'Mathematics', 'HTML 5', 'Economic Science', 'Dutch', 'German'];
 
   constructor(private userService: UserService, private authenticationService: AuthenticationService) {
@@ -42,11 +43,30 @@ export class CoachesOverviewComponent implements OnInit {
 
   }
 
-  filterCoaches(topic: string): void {
-    if (topic === '') {
+  filterCoaches(topics): void {
+    console.log(topics);
+    if (topics === '') {
       this.users = this.allTheCoaches;
     } else {
-      this.users = this.allTheCoaches.filter(user => user.coach.firstTopic === topic || user.coach.secondTopic === topic);
+      /*const collectItems = options => options.reduce((selectedLabels, { selected, label }) => {
+        if (selected) selectedLabels.push(label)
+        return selectedLabels;
+      }, []);*/
+
+      /*for (const topic of topics) {
+
+      }*/
+
+      this.users = this.allTheCoaches.filter(user => user.coach.firstTopic === topics || user.coach.secondTopic === topics);
+    }
+  }
+
+  filterByYear(year: string): void {
+    console.log(year);
+    if (year === '') {
+      this.users = this.allTheCoaches;
+    } else {
+      this.users = this.allTheCoaches.filter(user => user.coach.classesForFirstTopic === year || user.coach.classesForSecondTopic === year);
     }
   }
 
