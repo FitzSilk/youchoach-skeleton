dc0fa206bb522c044620fdcd00deb25da732a496
Cecilia Szenes <cecilia.szenes@gmail.com>
2020-05-27 10:08:00 +0200

    Story 10 getting topics in backend

---
+++ youcoach-backend/src/main/java/com/switchfully/youcoach/api/topics/TopicController.java
@@ -0,0 +1,30 @@
+package com.switchfully.youcoach.api.topics;
+
+
+import com.switchfully.youcoach.domain.topics.Topic;
+import com.switchfully.youcoach.service.topics.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping(path = TopicController.TOPIC_RESOURCE_PATH)
+public class TopicController {
+    private TopicService topicService;
+    public static final String TOPIC_RESOURCE_PATH = "/topic";
+
+    @Autowired
+    public TopicController(TopicService topicService) {
+        this.topicService = topicService;
+    }
+
+    @GetMapping(produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Topic> getAllTopics() {
+        return topicService.getAllTopics();
+    }
+}
