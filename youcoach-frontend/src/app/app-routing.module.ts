acbaa2f0ceb8a44063fabf18a783f8dc2407e14e
FitzSilk <60465804+FitzSilk@users.noreply.github.com>
2020-05-27 08:20:18 +0200

    A few css & html tweaks + remove hello-world component / endpoint.

--- youcoach-frontend/src/app/app-routing.module.ts
+++ youcoach-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {LoginComponent} from './login/login.component';
-import {HelloWorldComponent} from './hello-world/hello-world.component';
 import {HomeComponent} from './home/home.component';
 import {RegistrationComponent} from './registration/registration.component';
 import {MyprofileComponent} from './myprofile/myprofile.component';
@@ -17,7 +16,6 @@ const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'hello-world', component: HelloWorldComponent},
   {path: 'register', component: RegistrationComponent},
   {path: 'coaches', component: CoachesOverviewComponent},
   {path: 'user/editprofile', component: EditprofileComponent},
