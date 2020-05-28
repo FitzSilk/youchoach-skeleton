be3ae959078dcae3df8c11edb9bb83ffb9b4d251
Patrizia Battisti <patriziabattisti@hotmail.com>
2020-05-28 14:11:59 +0200

    coachee session overview

--- youcoach-frontend/src/app/coachee-session-overview/coachee-session-overview.component.ts
+++ youcoach-frontend/src/app/coachee-session-overview/coachee-session-overview.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Session} from '../classes/session';
+import {SessionService} from '../services/session.service';
+import {AuthenticationService} from '../authentication/authentication.service';
 
 @Component({
   selector: 'app-coachee-session-overview',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./coachee-session-overview.component.css']
 })
 export class CoacheeSessionOverviewComponent implements OnInit {
+  upcomingSessions: Session[] = [];
+  waitingForFeedbackSessions: Session[] = [];
+  archivedSessions: Session[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private sessionService: SessionService, private authenticationService: AuthenticationService) {
   }
 
+  ngOnInit(): void {
+    this.getSessions();
+  }
+
+  getSessions() {
+    const userId = this.authenticationService.getId();
+    this.sessionService.getCoacheeSessionByID(userId).subscribe(sessions => {
+      sessions.map(session => {
+        if (session.status === 'PENDING' || session.status === 'ACCEPTED') {
+          this.upcomingSessions.push(session);
+        } else if (session.status === 'WAITING_FEEDBACK') {
+          this.waitingForFeedbackSessions.push(session);
+        } else {
+          this.archivedSessions.push(session);
+        }
+      });
+    });
+  }
 }
